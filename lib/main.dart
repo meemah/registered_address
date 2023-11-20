3a0412f258a3eec4d1155d29e1d67ae18104344b
meemah <maimunahibro94@gmail.com>
2023-11-20 14:16:36 +0100

    added my info to notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:registered_address/view/residential_address/residential_address_view.dart';
+
+import 'view/residential_address/residential_address_view.dart';
 
 void main() {
   runApp(const MyApp());
